acc0056f383760b2938640549700cea0feed537b
Jack Tsai <jtsai0606@gmail.com>
2016-06-18 23:52:45 -0700

    setup to render static backbone stuff with node server

--- app/server.js
+++ app/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const express = require('express');
+const application_root = __dirname,
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  path = require("path");
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world!\n');
-});
+
+app.use(express.static(path.join(application_root, "public")));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello world!\n');
+// });
+
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
