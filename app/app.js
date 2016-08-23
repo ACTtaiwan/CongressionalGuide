141d2593ccfc483856e37142d07b3001482c5d39
terry77228 <terry.chintung.hsu@gmail.com>
2016-08-23 07:37:20 +0000

    Delete useless code

--- app/app.js
+++ app/app.js
@@ -26,7 +26,7 @@ var sunlight_legislator = require('./modules/sunlight_legislator');
 var PORT = 8080;
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db');
+var db = new sqlite3.Database('../db/db.sqlite3');
 db.serialize(function() {
   db.run('CREATE TABLE IF NOT EXISTS candidates(' +
     'firstName TEXT, ' +
@@ -54,148 +54,12 @@ db.serialize(function() {
   ')');
 
   db.run('CREATE TABLE IF NOT EXISTS cosponsors_bills(' +
-    'cid INTEGER PRIMARY KEY ASC, ' + 
+    'cid INTEGER PRIMARY KEY ASC, ' +
     'cosponsorId TEXT REFERENCES candidates (bioguideId), ' +
     'billId TEXT REFERENCES bills (billId)' +
   ')');
 });
 
-new CronJob('0 0 * * * *', function() {
-  var candidateFecIds = [];
-  var candidateBioguideIds =[];
-  // fecId should be a good unique ID for candidate
-  db.each("SELECT fecId, bioguideId FROM candidates", function(err, row) {
-    candidateFecIds.push(row.fecId);
-    candidateBioguideIds.push(row.bioguideId);
-  }, function() {
-  //  fetchCandidatesData('http://realtime.influenceexplorer.com/api/candidates/?format=json&page=1&apikey=' + SUNLIGHT_APIKEY, candidateFecIds);
-//    fetchBillsData(BILL_URL + BILL_RANGE + '&apikey=' + SUNLIGHT_APIKEY, candidateBioguideIds);
-  });
-}, null, true, 'America/Los_Angeles');
-
-
-
-function fetchBillsData(url, candidateBioguideIds){
-
-//	db.run('UPDATE candidates SET bills="ccc" WHERE district=1');
-	console.log(db.run('UPDATE candidates SET bills="ggg" WHERE district=1;'));
-/*
-  http.get(url, (res) => {
-    var jsonString = '';  
-    res.on('data', (d) => {
-      jsonString += d;
-    });
-    res.on('end', () => {
-      var data = JSON.parse(jsonString);
-      var bills = data.results;
-      
-      var stmt = db.prepare('UPDATE candidates ' +
-	'SET bills = $bills ' + 
-	'WHERE bioguideId = $bioguideId'
-	);
-
-      var bill_list = [];
-      for (var i = 0; i < bills.length; i++) {
-	var isFind = false;
-	var index = candidateBioguideIds.indexOf(bills[i].sponsor_id);
-	
-        if (index >= 0){
-		bill_list.push(bills[i].bill_id);
-		isFind = true;		
-	}
-	else{
-		for(var j = 0; j < bills[i].cosponsor_ids.length; ++j){
-			index = candidateBioguideIds.indexOf(bills[i].cosponsor_ids[j]);
-			if(index >= 0){
-				bill_list.push(bills[i].bill_id);
-				isFind = true;
-				break;
-			}
-		}
-	}
-	 if(!isFind)
-		continue;
-
-//	  console.log(JSON.stringify(bill_list));
-
-	  console.log(JSON.stringify(bill_list));
-	  console.log(candidateBioguideIds[index]);
-          stmt.run({
-            $bills: JSON.stringify(bill_list),
-	    $bioguideId: candidateBioguideIds[index]
-          });
-        
-      }
-      stmt.finalize();
-      if (data.next) {
-        fetchBillsData(data.next, candidateBioguideIds);
-      } else {
-        console.log('fetch supported bills completed');
-        db.close();
-      }
-    });
-  }).on('error', (e) => {
-    console.log('Got error: ${e.message}');
-  });
-*/
-}
-
-function fetchCandidatesData(url, existedCandidateFecIds) {
-  http.get(url, (res) => {
-    var jsonString = '';
-    res.on('data', (d) => {
-      jsonString += d;
-    });
-    res.on('end', () => {
-      var data = JSON.parse(jsonString);
-      var candidates = data.results;
-
-      var stmt = db.prepare('INSERT INTO candidates (firstName, lastName, party, chamber, state, district, incumbent, fecId) ' +
-        'VALUES ($firstName, $lastName, $party, $chamber, $state, $district, $incumbent, $fecId)');
-      for (var i = 0; i < candidates.length; i++) {
-        if ((candidates[i].office === 'H' || candidates[i].office === 'S') &&
-            candidates[i].election_year == 2016 && 
-            existedCandidateFecIds.indexOf(candidates[i].fec_id) < 0) {
-          console.log('adding new candidate: ' + candidates[i].name);
-          var name = candidates[i].name.split(',');
-          var lastName, firstName;
-          if (name.length === 2) {
-            lastName = toTitleCase(name[0].trim());
-            firstName = toTitleCase(name[1].trim());
-          } else {
-            lastName = null;
-            firstName = name;
-          }
-
-          stmt.run({
-            $firstName: firstName,
-            $lastName: lastName,
-            $party: candidates[i].party,
-            $chamber: candidates[i].office,
-            $state: candidates[i].state,
-            $district: candidates[i].office_district,
-            $incumbent: candidates[i].is_incumbent,
-            $fecId: candidates[i].fec_id
-          });
-        }
-      }
-      stmt.finalize();
-
-      if (data.next) {
-        fetchCandidatesData(data.next, existedCandidateFecIds);
-      } else {
-        console.log('fetch candidates completed');
-        db.close();
-      }
-    });
-  }).on('error', (e) => {
-    console.log('Got error: ${e.message}');
-  });
-}
-
-function toTitleCase(str) {
-  return str.replace(/\b\w+/g,function(s){return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();});
-}
 
 
 // view engine setup
