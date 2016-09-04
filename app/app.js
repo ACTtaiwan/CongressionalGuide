c8789e36fc2655302657a853191a2ad526bc3220
Brock Freeman <brockfreeman@kosah.com>
2016-09-03 19:13:52 -0700

    Brought back but commented out code for getting legislators info from Sunlight. Fixed spelling issue.

--- app/app.js
+++ app/app.js
@@ -65,7 +65,76 @@ db.serialize(function() {
   ')');
 });
 
-
+// Run midnight job to fetch 
+// new CronJob('0 0 * * * *', function() {
+//   var candidateFecIds = [];
+//   fecId should be a good unique ID for candidate
+//   db.each("SELECT fecId FROM candidates", function(err, row) {
+//     candidateFecIds.push(row.fecId);
+//   }, function() {
+// 	fetchCandidatesData(
+// 	'http://realtime.influenceexplorer.com/api/candidates/?format=json&page=1&apikey=' + 
+// 	SUNLIGHT_APIKEY, candidateFecIds);
+//   });
+// }, null, true, 'America/Los_Angeles');
+// 
+// function fetchCandidatesData(url, existedCandidateFecIds) {
+// http.get(url, (res) => {
+//  var jsonString = '';
+//  res.on('data', (d) => {
+//    jsonString += d;
+//  });
+//  res.on('end', () => {
+//    var data = JSON.parse(jsonString);
+//    var candidates = data.results;
+// 
+//    var stmt = db.prepare('INSERT INTO candidates (firstName, lastName, party, chamber, state, district, incumbent, fecId) ' +
+// 	 'VALUES ($firstName, $lastName, $party, $chamber, $state, $district, $incumbent, $fecId)');
+//    for (var i = 0; i < candidates.length; i++) {
+// 	 if ((candidates[i].office === 'H' || candidates[i].office === 'S') &&
+// 		 candidates[i].election_year == 2016 && 
+// 		 existedCandidateFecIds.indexOf(candidates[i].fec_id) < 0) {
+// 	   console.log('adding new candidate: ' + candidates[i].name);
+// 	   var name = candidates[i].name.split(',');
+// 	   var lastName, firstName;
+// 	   if (name.length === 2) {
+// 		 lastName = toTitleCase(name[0].trim());
+// 		 firstName = toTitleCase(name[1].trim());
+// 	   } else {
+// 		 lastName = null;
+// 		 firstName = name;
+// 	   }
+// 
+// 	   stmt.run({
+// 		 $firstName: firstName,
+// 		 $lastName: lastName,
+// 		 $party: candidates[i].party,
+// 		 $chamber: candidates[i].office,
+// 		 $state: candidates[i].state,
+// 		 $district: candidates[i].office_district,
+// 		 $incumbent: candidates[i].is_incumbent,
+// 		 $fecId: candidates[i].fec_id
+// 	   });
+// 	 }
+//    }
+//    stmt.finalize();
+// 
+//    if (data.next) {
+// 	 fetchCandidatesData(data.next, existedCandidateFecIds);
+//    } else {
+// 	 console.log('fetch candidates completed');
+// 	 db.close();
+//    }
+//  });
+// }).on('error', (e) => {
+//  console.log(`Got error: ${e.message}`);
+// });
+// }
+//  
+// function toTitleCase(str) {
+// 	return str.replace(/\b\w+/g,function(s){return s.charAt(0).toUpperCase() + 
+// 		s.substr(1).toLowerCase();});
+// }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
