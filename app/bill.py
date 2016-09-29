fd4a4fe67c2bfa7e332a5d4a0b648ae39bea80df
Brock Freeman <brockfreeman@kosah.com>
2016-09-28 19:41:53 -0700

    Get bill_type and number from Sunlight; update some variable names.

--- app/bill.py
+++ app/bill.py
@@ -3,9 +3,15 @@ import sqlite3, json, os
 import logging, sys
 import requests
 
+# !!! UPDATE HERE WHENEVER THE DATABASE TABLE SCHEMA CHANGE !!!
+#
+# The order matter when we want to insert the value, current schema:
+# CREATE TABLE bills (billId TEXT PRIMARY KEY REFERENCES cosponsors_bills (billId), bill_type TEXT, number INTEGER, officialTitle BLOB, sponsorId TEXT, url TEXT UNIQUE);
+
+
 logging.basicConfig(stream=sys.stderr,level=logging.DEBUG)
 
-URL = 'https://congress.api.sunlightfoundation.com/bills?apikey=d9eeb169a7224fe28ae0f32aca0dc93e&bill_id__in=sconres38-114%7Chconres88-114%7Cs2426-114%7Chr4154-114%7Chr1853-114%7Chconres76-114%7Cs1683-113%7Chres494-113%7Chjres109-113%7Csjres31-113%7Chconres55-113%7Chconres46-113%7Chr1151-113%7Cs579-113%7Chres185-113%7Chconres29-113%7Cs12-113%7Chr419-113&fields=official_title,urls.congress,sponsor_id,cosponsor_ids'
+URL = 'https://congress.api.sunlightfoundation.com/bills?apikey=d9eeb169a7224fe28ae0f32aca0dc93e&bill_id__in=hr6047-114%7Csconres38-114%7Chconres88-114%7Cs2426-114%7Chr4154-114%7Chr1853-114%7Chconres76-114%7Cs1683-113%7Chres494-113%7Chjres109-113%7Csjres31-113%7Chconres55-113%7Chconres46-113%7Chr1151-113%7Cs579-113%7Chres185-113%7Chconres29-113%7Cs12-113%7Chr419-113&fields=bill_type,number,official_title,urls.congress,sponsor_id,cosponsor_ids'
 
 dbpath = '/root/CongressionalGuide/db/db.sqlite3'
 if not (dbpath and os.path.isfile(dbpath)):
@@ -19,7 +25,7 @@ except sqlite3.Error:
   print 'sqlite3 error'
   db.close()
 
-bills_insert_query = 'INSERT INTO bills VALUES (?,?,?,?)'
+bills_insert_query = 'INSERT INTO bills VALUES (?,?,?,?,?,?)'
 cosponsors_bills_insert_query = 'INSERT INTO cosponsors_bills VALUES (?,?,?)'
 
 r = requests.get(URL)
@@ -29,39 +35,47 @@ ai = (None,) # auto increment
 for i in js['results']:
 
     # prepare data for db
-    bid=(None,)
-    ot=(None,)
-    sid=(None,)
+    billid=(None,)
+    title=(None,)
+    sponsorid=(None,)
     url=(None,)
-    cid_list = () 
+    cosponsorid_list = ()
+    bill_type = (None,)
+    number = (None)    
 
     for k,v in i.iteritems():
         if k == 'bill_id':
             logging.info('bill_id: %s', v)
-            bid = v,
+            billid = v,
         elif k == 'cosponsor_ids':
             logging.info('cosponsor_ids LIST: %s', v)
-            for cid in v:
-                logging.info('cosponsor_ids ITEM: %s', cid)
-                cid_list += cid,
+            for cosponsorid in v:
+                logging.info('cosponsor_ids ITEM: %s', cosponsorid)
+                cosponsorid_list += cosponsorid,
         elif k == 'urls':
             logging.info('url: %s', v)
             url = v['congress'],
         elif k == 'sponsor_id':
             logging.info('sponsor_id: %s', v)
-            sid = v,
+            sponsorid = v,
         elif k == 'official_title':
             logging.info('official_title: %s', v)
-            ot = v,
+            title = v,
+        elif k == 'bill_type':
+        	logging.info('bill_type: %s', v)
+        	bill_type = v,
+        elif k == 'number':
+        	logging.info('number: %s', v)
+        	number = v,
         else:
             logging.warning('unknown key %s', k)        
         
-    bills_insert_values = (bid + ot + sid + url)
+    bills_insert_values = (billid + bill_type + number + title + sponsorid + url)
     logging.info('in 1: %s', bills_insert_values)
     c.execute(bills_insert_query, bills_insert_values)
 
-    for cd in cid_list:
-        cosponsors_bills_insert_values = (ai + tuple([cd]) + bid)
+    for cd in cosponsorid_list:
+        cosponsors_bills_insert_values = (ai + tuple([cd]) + billid)
         logging.info('in 2: %s', cosponsors_bills_insert_values)
         c.execute(cosponsors_bills_insert_query, cosponsors_bills_insert_values)
     
